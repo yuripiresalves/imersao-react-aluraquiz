b769d7903c16c9a0346bf6ce9597adf0fc758c68
Yuri Alves <yuripiresalves@gmail.com>
2021-01-26 16:18:04 -0300

    segundo dia finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 
@@ -24,15 +28,37 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - BABYMETAL</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>BABYMETAL</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Descirção</p>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+
+                router.push(`/quiz?name=${name}`);
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Diz aí seu nome"
+                onChange={({ target }) => setName(target.value)}
+              />
+              <button type="submit" disabled={name.length == 0}>
+                Bora jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
---
+++ pages/quiz.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const quiz = () => {
+  return <div>Meu quiz mamaco</div>;
+};
+
+export default quiz;
